fa2eb391993c3e67e437b56e482c66195d059b87
Meng Jia <jiameng_73@hotmail.com>
2018-03-22 22:48:02 -0400

    added wallaby support

--- src/App.vue
+++ src/App.vue
@@ -8,28 +8,28 @@
 </template>
 
 <script>
-import Vue from 'vue'
-import Component from 'vue-class-component'
+// import Vue from 'vue'
+// import Component from 'vue-class-component'
 
-@Component({
-  props: {
-    msg: {
-      default: 'from app.vue'
-    }
-  }
-})
-export default class App extends Vue {
-  message = 'Hello from Class'
+// @Component({
+//   props: {
+//     msg: {
+//       default: 'from app.vue'
+//     }
+//   }
+// })
+export default {
+  name: 'app'
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ wallaby.js
@@ -0,0 +1,39 @@
+const webpackConfig = require('./build/webpack.test.conf.js')
+const wallabyWebpack = require('wallaby-webpack')
+
+module.exports = function (wallaby) {
+
+  return {
+    files: [
+      'src/**/*.*',
+      '!src/**/*.test.js'
+    ],
+
+    tests: [
+      'test/**/*.spec.js',
+      'src/**/*.test.js'
+    ],
+
+    env: {
+      type: 'node',
+      runner: 'node'
+    },
+
+    compilers: {
+      '**/*.js': wallaby.compilers.babel(),
+      '**/*.vue': require('wallaby-vue-compiler')(wallaby.compilers.babel({}))
+    },
+
+    testFramework: 'jest',
+
+    setup: function (wallaby) {
+      const jestConfig = require('./package.json').jest;
+      jestConfig.moduleNameMapper = {
+        "^@/(.*)$": wallaby.projectCacheDir + "/src/$1"
+      };
+      wallaby.testFramework.configure(jestConfig);
+    },
+
+    debug: true
+  };
+};
